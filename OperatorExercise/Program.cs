9b26a6bb48fc46184936f447f09402e46340c5d9
Andrew Bayer <andrewbayer21@gmail.com>
2022-12-27 07:01:53 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = YourArea(radius);
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {areaOfCircle}.");
+
+
         }
+
+        public static double YourArea(double radius)
+        {
+
+            return Math.PI * (radius * radius);
+
+        }
+
+
+
     }
+     
+
 }
